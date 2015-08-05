9b5be330d0f2a1d01e4df351c9ff2869bf67aa8e
Dragon Dave McKee <dragon@scraperwiki.com>
2015-08-05 23:48:31 +0100

    radical overhaul

--- capacitor.lua
+++ capacitor.lua
@@ -1,58 +1,38 @@
 -- from http://www.computercraft.info/forums2/index.php?/topic/22157-ender-io-capacitor-bank-monitoring/
 local capacitor_1 = peripheral.wrap("right")
-local modem = peripheral.wrap("back")
 local monitor = peripheral.wrap("top")
 monitor.clear() --paint the background
-i = 1
 --percentage math constants
 makePercent = 100
-capacitorCount = 16
-capacitorBasic = 1000000
-capacitorStandard = 5000000
-capacitorVibrant = 25000000
-totalCapacity = capacitorCount * capacitorStandard
-fillPercentage = 0
-difference = 0
+fill = 1
+stop_fill = 40e6
+start_fill = 10e6
+totalCapacity = 50e6
 while true do
   energyLevel1 = capacitor_1.getEnergyStored()
   energyAdjusted = (energyLevel1) * (capacitorCount)
-  --print(energyAdjusted)
-  fillLevel = (4500000) * (capacitorCount)
-  lowFillLevel = (3000000) * (capacitorCount)
-  if energyAdjusted > fillLevel then
-        redstone.setOutput("bottom", true)
-        monitor.setCursorPos(6, 5)
-        --monitor.setTextColor(16384) --text will be red
-        monitor.write("Generators Inactive")
+  print(energyAdjusted)
+  monitor.setCursorPos(6, 5)
+  if energyAdjusted > stop_fill then
+    fill = 0
+    monitor.write("Generator: off")
+  elseif energyAdjusted < start_fill then
+    fill = 1
+    monitor.write("Generator: on")
   end
-  if redstone.getOutput("bottom") == true then
-        monitor.setCursorPos(6, 5)
-        --monitor.setTextColor(16384) --text will be red
-        monitor.write("Generators Inactive")
-  end
-  if redstone.getOutput("bottom") == false then
-        monitor.setCursorPos(7,5)
-        --monitor.setTextColor(8192) --text will be green
-        monitor.write("Generators Active")
-  end
-  if energyAdjusted < lowFillLevel then
-        redstone.setOutput("bottom", false)
-        monitor.setCursorPos(7,5)
-        --monitor.setTextColor(8192) --text will be green
-        monitor.write("Generators Active")
-  end
+  
+  if fill == 1 then
+    redstone.setOutput("bottom", true)
+  else
+    redstone.setOutput("bottom", false)
+  
   monitor.setCursorPos(1,1)
-  print("Generators with start at "..lowFillLevel.." RF")
   print("Current energy storage is "..energyAdjusted.." RF")
-  --monitor.write("Current energy storage is "..energyAdjusted.." RF")
-  difference = (totalCapacity) - (energyAdjusted)
   fillAmount = (energyAdjusted) / (totalCapacity)
   fillPercentage = (fillAmount) * (100)
   fillInt = math.floor(fillPercentage)
   monitor.setCursorPos(3,2)
-  --monitor.setTextColor(1)
   monitor.write("Currently at "..fillInt.."% capacity")
-  --monitor.write(string.format("%.2fillPercentage", fillPercentage))
   sleep(2.5)
   monitor.clear()
 end
