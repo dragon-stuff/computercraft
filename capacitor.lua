9d426753c4d1080c1adb978cca3c7f7b5a43669a
Dragon Dave McKee <dragon@scraperwiki.com>
2015-08-06 00:51:00 +0100

    Delta

--- capacitor.lua
+++ capacitor.lua
@@ -4,13 +4,16 @@ local monitor = peripheral.wrap("top")
 monitor.clear() --paint the background
 --percentage math constants
 makePercent = 100
+last = 0
 fill = 1
 totalCapacity = 80e6
 stop_fill = totalCapacity*0.9
 start_fill = totalCapacity*0.2
+sleep_time = 5 -- seconds between cpu updates
 
 while true do
   energyLevel1 = capacitor_1.getEnergyStored()
+  diff = energyLevel1 - last
   energyAdjusted = (energyLevel1) * (capacitorCount)
   if energyAdjusted > stop_fill then
     fill = 0
@@ -19,6 +22,15 @@ while true do
     fill = 1
     print "Underfill"
   end
+  
+  fillAmount = (energyAdjusted) / (totalCapacity)
+  fillPercentage = (fillAmount) * (100)
+  fillInt = math.floor(fillPercentage)
+  monitor.setCursorPos(1,1)
+  monitor.write(math.floor(energyAdjusted/1e6).."M RF")
+  
+  monitor.setCursorPos(1,2)
+  monitor.write(fillInt.."%")
   
   monitor.setCursorPos(1,3)
   if fill == 1 then
@@ -29,13 +41,9 @@ while true do
     monitor.write("Off")
   end
   
-  monitor.setCursorPos(1,1)
-  monitor.write(math.floor(energyAdjusted/1e6).."M RF")
-  fillAmount = (energyAdjusted) / (totalCapacity)
-  fillPercentage = (fillAmount) * (100)
-  fillInt = math.floor(fillPercentage)
-  monitor.setCursorPos(1,2)
-  monitor.write(fillInt.."%")
-  sleep(5)
+  monitor.setCursorPos(1,4)
+  monitor.write("Del "..math.floor(diff/sleeptime))
+  
+  sleep(sleeptime)
   monitor.clear()
 end
